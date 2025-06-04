cec209b04f2eaa4c1fe4965ef80caab02c016235
2Doyoung <dlehdud1653@kakao.com>
2025-06-04 19:18:23 +0900

    redis

--- src/main/java/com/example/redis/controller/RedisController.java
+++ src/main/java/com/example/redis/controller/RedisController.java
@@ -1,8 +1,14 @@
 package com.example.redis.controller;
+import com.example.redis.service.RedisService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/redis")
 public class RedisController {
+    private final RedisService redisService;
+
+    public RedisController(RedisService redisService) {
+        this.redisService = redisService;
+    }
 }
