732714182f62477ccd1d4ac991f94e4c93465260
2Doyoung <dlehdud1653@kakao.com>
2025-06-05 20:01:24 +0900

    redis service

--- src/main/java/com/example/redis/service/RedisService.java
+++ src/main/java/com/example/redis/service/RedisService.java
@@ -1,7 +1,28 @@
 package com.example.redis.service;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RedisService {
+    private final StringRedisTemplate redisTemplate;
+
+    public RedisService(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    // 저장
+    public void saveData(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    // 조회
+    public String getData(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    // 삭제
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
 }
