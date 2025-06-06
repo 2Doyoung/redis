dc49ab29eed99a712c1047caef5553f6169d2f58
2Doyoung <dlehdud1653@kakao.com>
2025-06-06 14:18:26 +0900

    redis

--- src/main/java/com/example/redis/controller/RedisController.java
+++ src/main/java/com/example/redis/controller/RedisController.java
@@ -1,7 +1,7 @@
 package com.example.redis.controller;
 import com.example.redis.service.RedisService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/redis")
@@ -10,5 +10,23 @@ public class RedisController {
 
     public RedisController(RedisService redisService) {
         this.redisService = redisService;
+    }
+
+    @PostMapping("/set")
+    public ResponseEntity<String> set(@RequestParam String key, @RequestParam String value) {
+        redisService.saveData(key, value);
+        return ResponseEntity.ok("저장 완료");
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<String> get(@RequestParam String key) {
+        String value = redisService.getData(key);
+        return ResponseEntity.ok("값: " + value);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> delete(@RequestParam String key) {
+        redisService.deleteData(key);
+        return ResponseEntity.ok("삭제 완료");
     }
 }
